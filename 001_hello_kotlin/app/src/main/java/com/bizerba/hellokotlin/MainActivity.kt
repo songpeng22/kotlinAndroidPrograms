c2a2c3ea54eaff130eaee4849c8062b2766892c5
peng <songpeng24@msn.com>
2023-06-14 09:58:15 +0800

    Log.v in Kotlin

--- 001_hello_kotlin/app/src/main/java/com/bizerba/hellokotlin/MainActivity.kt
+++ 001_hello_kotlin/app/src/main/java/com/bizerba/hellokotlin/MainActivity.kt
@@ -3,9 +3,15 @@ package com.bizerba.hellokotlin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import android.util.Log
+
 class MainActivity : AppCompatActivity() {
+    private var TAG : String = "Hello_Kotlin"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.v(TAG,"Hello Log.v.")
+        println("Hello println.")
     }
 }

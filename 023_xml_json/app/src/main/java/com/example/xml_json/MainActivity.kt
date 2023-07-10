dfabb9cfc5b906da334ba63f0a5f6a1ff49fb681
peng <songpeng24@msn.com>
2023-07-10 16:11:36 +0800

    Jackson to parse json to tree

--- 023_xml_json/app/src/main/java/com/example/xml_json/MainActivity.kt
+++ 023_xml_json/app/src/main/java/com/example/xml_json/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.example.xml_json
 
+import android.R.xml
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.xml.JSONObject
 import org.xml.XML
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        /*
+         * org.json
+         * */
         val jsonObject:JSONObject = JSONObject()
         jsonObject.put("Name", "Robert")
         jsonObject.put("ID", 1)
@@ -17,14 +24,23 @@ class MainActivity : AppCompatActivity() {
         jsonObject.put("Active", true)
         jsonObject.put("Details", JSONObject.NULL)
 
-
         //Convert a JSONObject to XML
         val xmlText:String = XML.toString(jsonObject)
         println("xml:")
         println(xmlText)
 
         //Convert an XML to JSONObject
+        val jsonText:String = XML.toJSONObject(xmlText).toString()
         println("json:")
-        println(XML.toJSONObject(xmlText))
+        println(jsonText)
+
+        /*
+         * Jackson
+         * */
+        val mapper = ObjectMapper()
+        val actualObj:JsonNode = mapper.readTree(jsonText)
+        println("actualObj:Name:${actualObj.get("Name")}")
+        println("actualObj:Active:${actualObj.get("Active")}")
+
     }
 }

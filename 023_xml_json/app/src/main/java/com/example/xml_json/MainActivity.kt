11424df98360acb9b2730d6e03df4f15eb8c5512
peng <songpeng24@msn.com>
2023-07-13 16:00:59 +0800

    dom && subNode.forEachIndexed

--- 023_xml_json/app/src/main/java/com/example/xml_json/MainActivity.kt
+++ 023_xml_json/app/src/main/java/com/example/xml_json/MainActivity.kt
@@ -6,16 +6,43 @@ import com.ctc.wstx.stax.WstxInputFactory
 import com.ctc.wstx.stax.WstxOutputFactory
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.xml.XmlFactory
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
+import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
 import org.xml.JSONObject
 import org.xml.XML
+import java.io.ByteArrayInputStream
+import java.io.File
+import java.io.InputStream
+import java.nio.charset.StandardCharsets
+import javax.xml.parsers.DocumentBuilderFactory
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val xmlTreeStr:String = """<categories>
+                                <category>abc
+                                    <category>cde
+                                        <item>someid_1</item>
+                                        <item>someid_2</item>
+                                        <item>someid_3</item>
+                                        <item>someid_4</item>
+                                    </category>
+                                </category>
+                                <category>xyz
+                                   <category>zwd
+                                      <category>hgw
+                                         <item>someid_5</item>
+                                      </category>
+                                   </category>
+                                </category>
+                             </categories>"""
 
         /*
          * org.json
@@ -49,13 +76,32 @@ class MainActivity : AppCompatActivity() {
         /*
          * Jackson
          * */
+        //
         //read json to tree
+        //
         val mapper = ObjectMapper()
         var actualJsonObj:JsonNode = mapper.readTree(jsonText)
         println("actualObj:Name:${actualJsonObj.get("Name")}")
         println("actualObj:Active:${actualJsonObj.get("Active")}")
+        println("actualObj:toPrettyString:${actualJsonObj.toPrettyString()}")
+        println("actualObj:tree view:${org.json.JSONObject(jsonText).toString(2)}")
+        val subNode1 = actualJsonObj.at("/Name")
+        if(subNode1 != null && !subNode1.isEmpty){
+            val subNodeStr = mapper.writeValueAsString(subNode1)
+            println("actualObj:tree view:${org.json.JSONObject(subNodeStr).toString(2)}")
+        }
+        else if(subNode1 == null){
+            println("subNode is null")
+        }
+        else if(subNode1.isEmpty){
+            println("subNode is empty:${subNode1}")
+        }
+        val subNode2 = actualJsonObj.path("Name")
+        println("subNode2 is:${subNode2.toPrettyString()}")
 
+        //
         //read xml to tree: failed, no error, but result of getName is null
+        //
         //implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.13.3'
         //success at
 //        implementation 'com.fasterxml.jackson.core:jackson-core:2.14.2'
@@ -64,15 +110,61 @@ class MainActivity : AppCompatActivity() {
 //        implementation 'com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.14.2'
 //        implementation 'com.fasterxml.woodstox:woodstox-core:6.5.0'
 //        implementation 'javax.xml.stream:stax-api:1.0-2'
-        val xml:String = ("<?xml version='1.0'?><Root><Name>Robert</Name><Active>true</Active></Root>")
+        val xml:String = ("<?xml version='1.0'?><dataroot><Name type=\"human\">Robert</Name><Active>true</Active><Name>Robin</Name><Active>false</Active></dataroot>")
         val xmlFactory = XmlFactory.builder()
             .xmlInputFactory(WstxInputFactory())
             .xmlOutputFactory(WstxOutputFactory())
             .build()
         val xmlMapper:XmlMapper = XmlMapper.builder(xmlFactory).build()
         var actualXmlObj = xmlMapper.readTree(xml)
+        //xml tree view
+        xmlMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        var prettyXmlStr = xmlMapper.writeValueAsString(actualXmlObj)
+        println("Jackson:xml tree view:${prettyXmlStr}")
+        //Jackson:xml to json
+        val jsonMapper:ObjectMapper = ObjectMapper()
+        val jsonStr:String = jsonMapper.writeValueAsString(actualXmlObj)
+        println("Jackson:xml to json:${jsonStr}")
+        //json tree view
+        println("actualXmlObj:tree view:${org.json.JSONObject(jsonStr).toString(2)}")
         println("actualXmlObj:Name:${actualXmlObj.get("Name")}")
-        println("actualXmlObj:Active:${actualXmlObj.get("Active")}")   //result is null
+        println("actualXmlObj:Active:${actualXmlObj.get("Active")}")
+
+        //
+        //read xml InputStream to tree
+        //but with the same tag, only first tag is identified by object
+        //
+        val xml2:String = ("<?xml version='1.0'?>" +
+                "<Root>" +
+                    "<Layout>" +
+                        "<Field ID=\"31\" TYPE=\"6\"></Field>" +
+                        "<Field ID=\"32\" TYPE=\"6\"></Field>" +
+                        "<VARLIST>" +
+                            "<VARIABLE VARID=\"913\" FIELDTYPE=\"13\"></VARIABLE>" +
+                            "<VARIABLE VARID=\"217\" FIELDTYPE=\"11\"></VARIABLE>" +
+                        "</VARLIST>" +
+                    "</Layout>" +
+                "</Root>")
+        val inputStream: InputStream =
+            ByteArrayInputStream(xml2.toByteArray(StandardCharsets.UTF_8))
+        val xmlFactory2 = XmlFactory.builder()
+            .xmlInputFactory(WstxInputFactory())
+            .xmlOutputFactory(WstxOutputFactory())
+            .build()
+        val xmlMapper2:XmlMapper = XmlMapper.builder(xmlFactory2).build()
+        var actualXmlObj2 = xmlMapper.readTree(inputStream)
+        actualXmlObj2.forEachIndexed  { index, node -> println("index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
+        //print xml path - /Layout
+        var xmlBranchPath:String = "/Layout"
+        var subNode = actualXmlObj2.at(xmlBranchPath)
+        println("Jackson:subNode:${xmlBranchPath}:${subNode}")
+        subNode.forEachIndexed  { index, node -> println("index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
+        //print xml path - /Field
+        xmlBranchPath = "/Layout/Field"
+        subNode = actualXmlObj2.at(xmlBranchPath)
+        println("Jackson:subNode:${xmlBranchPath}:${subNode},isObject:${subNode.isObject},isArray:${subNode.isArray},isValueNode:${subNode.isValueNode}")
+        subNode.forEachIndexed  { index, node -> println("index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
+
 
         //read xml to tree
         //java.lang.NoSuchMethodError: No static method newFactory(Ljava/lang/String;Ljava/lang/ClassLoader;)
@@ -80,10 +172,36 @@ class MainActivity : AppCompatActivity() {
         // or its super classes (declaration of 'javax.xml.stream.XMLInputFactory'
         //fix may be here: https://stackoverflow.com/questions/31360025/using-jackson-dataformat-xml-on-android
         //but will not touch the fix for now
-//        val xml2:String = ("<?xml version='1.0'?><Root><Name>Robert</Name><Active>true</Active></Root>")
+//        val xml3:String = ("<?xml version='1.0'?><Root><Name>Robert</Name><Active>true</Active></Root>")
 //        val xmlMapper2:XmlMapper = XmlMapper()
-//        var actualXmlObj2 = xmlMapper2.readTree(xml2)
-//        println("actualXmlObj2:Name:${actualXmlObj2.get("Name")}")
-//        println("actualXmlObj2:Active:${actualXmlObj2.get("Active")}")
+//        var actualXmlObj3 = xmlMapper2.readTree(xml3)
+//        println("actualXmlObj3:Name:${actualXmlObj3.get("Name")}")
+//        println("actualXmlObj3:Active:${actualXmlObj3.get("Active")}")
+
+        //iterate root
+        val xmlFactory3 = XmlFactory.builder()
+            .xmlInputFactory(WstxInputFactory())
+            .xmlOutputFactory(WstxOutputFactory())
+            .build()
+        val xmlMapper3:XmlMapper = XmlMapper.builder(xmlFactory3).build()
+        var actualXmlObj3 = xmlMapper3.readTree(xmlTreeStr)
+        actualXmlObj3.forEachIndexed  { index, node -> println("index:${index},node:${node}") }
+
+        /*
+         * Dom parser
+         * */
+        //parse tag with same name
+        val f:File = File(createTempFile().toString())
+        f.writeText(xmlTreeStr)
+        val dbf = DocumentBuilderFactory.newInstance()
+        val db = dbf.newDocumentBuilder()
+        val doc: Document = db.parse(f)
+        val root: Element = doc.getDocumentElement()
+        val nodeList: NodeList = doc.getElementsByTagName("item")
+        for (i in 0 until nodeList.length) {
+            val node = nodeList.item(i) // this is node under track info
+
+            println("node name:${node.nodeName},value:${node.textContent}")
+        }
     }
 }

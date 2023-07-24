e179ddc0d478c11361b2c12ab4b1540f2179ece5
peng <songpeng24@msn.com>
2023-07-24 08:38:30 +0800

    nested unpack

--- 023_xml_json/app/src/main/java/com/example/xml_json/MainActivity.kt
+++ 023_xml_json/app/src/main/java/com/example/xml_json/MainActivity.kt
@@ -22,6 +22,32 @@ import java.nio.charset.StandardCharsets
 import javax.xml.parsers.DocumentBuilderFactory
 
 
+object  Unpack {
+    fun init(){
+
+    }
+    fun unpackNested(xmlBranchPath:List<String>,jsonNode: JsonNode,level:Int){
+        var path:String = xmlBranchPath.get(level)
+        println("Unpack::path:${path}")
+        var subNode = jsonNode.at(path)
+        var levelInner = level + 1
+        when(subNode.nodeType){
+            JsonNodeType.OBJECT -> println("Unpack::${subNode.toPrettyString()}")
+            JsonNodeType.ARRAY -> subNode.forEach{
+                println("Unpack::${it.toPrettyString()}")
+                if(levelInner < xmlBranchPath.size)
+                    unpackNested(xmlBranchPath,it,levelInner)
+            }
+            else -> {
+                println("subNode.nodeType:${subNode.nodeType}")
+            }
+        }
+    }
+
+
+
+}
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -86,7 +112,7 @@ class MainActivity : AppCompatActivity() {
         println("actualObj:Active:${actualJsonObj.get("Active")}")
         println("actualObj:toPrettyString:${actualJsonObj.toPrettyString()}")
         println("actualObj:tree view:${org.json.JSONObject(jsonText).toString(2)}")
-        val subNode1 = actualJsonObj.at("/Name")
+        var subNode1 = actualJsonObj.at("/Name")
         if(subNode1 != null && !subNode1.isEmpty){
             val subNodeStr = mapper.writeValueAsString(subNode1)
             println("actualObj:tree view:${org.json.JSONObject(subNodeStr).toString(2)}")
@@ -97,8 +123,8 @@ class MainActivity : AppCompatActivity() {
         else if(subNode1.isEmpty){
             println("subNode is empty:${subNode1}")
         }
-        val subNode2 = actualJsonObj.path("Name")
-        println("subNode2 is:${subNode2.toPrettyString()}")
+        subNode1 = actualJsonObj.path("Name")
+        println("subNode2 is:${subNode1.toPrettyString()}")
 
         //
         //read xml to tree: failed, no error, but result of getName is null
@@ -135,6 +161,7 @@ class MainActivity : AppCompatActivity() {
         //read xml InputStream to tree
         //but with the same tag, only first tag is identified by object
         //
+        println("xml2 begin:")
         val xml2:String = ("<?xml version='1.0'?>" +
                 "<Root>" +
                     "<Layout>" +
@@ -153,22 +180,32 @@ class MainActivity : AppCompatActivity() {
             .xmlOutputFactory(WstxOutputFactory())
             .build()
         val xmlMapper2:XmlMapper = XmlMapper.builder(xmlFactory2).build()
-        var actualXmlObj2 = xmlMapper.readTree(inputStream)
+        var actualXmlObj2 = xmlMapper2.readTree(inputStream)
         actualXmlObj2.forEachIndexed  { index, node -> println("index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
         //print xml path - /Layout
         var xmlBranchPath:String = "/Layout"
-        var subNode = actualXmlObj2.at(xmlBranchPath)
-        println("Jackson:subNode:${xmlBranchPath}:${subNode}")
-        subNode.forEachIndexed  { index, node -> println("index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
+        var subNode2 = actualXmlObj2.at(xmlBranchPath)
+        println("Jackson:subNode:${xmlBranchPath}:${subNode2}")
+        subNode2.forEachIndexed  { index, node -> println("subNode2::index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
         //print xml path - /Field
         xmlBranchPath = "/Layout/Field"
-        subNode = actualXmlObj2.at(xmlBranchPath)
-        println("Jackson:subNode:${xmlBranchPath}:${subNode},isObject:${subNode.isObject},isArray:${subNode.isArray},isValueNode:${subNode.isValueNode}")
-        subNode.forEachIndexed  { index, node -> println("index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
+        subNode2 = actualXmlObj2.at(xmlBranchPath)
+        println("Jackson:subNode:${xmlBranchPath}:${subNode2},isObject:${subNode2.isObject},isArray:${subNode2.isArray},isValueNode:${subNode2.isValueNode}")
+        subNode2.forEachIndexed  { index, node -> println("subNode2::index:${index},node:${node},isObject:${node.isObject},isArray:${node.isArray},isValueNode:${node.isValueNode}") }
         //check fake node
         xmlBranchPath = "/LAYOUT/NOTHING"
         var nodeOfNothing = actualXmlObj2.at(xmlBranchPath)
+        //nodeOfNothing.required("abc") //require series will generate exception if element not found
         println("nodeOfNothing.nodeType:${nodeOfNothing.nodeType}")
+        //find value
+        var nodeList1:List<JsonNode> = actualXmlObj2.findValues("Field")
+        println("nodeList1.size:${nodeList1.size}")
+        println("nodeList1.count:${nodeList1.count()}")
+        nodeList1.forEach{
+            println("it.toPrettyString():${it.toPrettyString()}")
+        }
+        println("xml2 end.")
+
 
         //read xml to tree
         //java.lang.NoSuchMethodError: No static method newFactory(Ljava/lang/String;Ljava/lang/ClassLoader;)
@@ -190,6 +227,65 @@ class MainActivity : AppCompatActivity() {
         val xmlMapper3:XmlMapper = XmlMapper.builder(xmlFactory3).build()
         var actualXmlObj3 = xmlMapper3.readTree(xmlTreeStr)
         actualXmlObj3.forEachIndexed  { index, node -> println("index:${index},node:${node}") }
+
+        //complex xml path
+        println("xml4 begin:complex xml path:")
+        val xml4:String = ("<?xml version='1.0'?>" +
+                "<Root>" +
+                    "<Layout>" +
+                        "<Field ID=\"31\" TYPE=\"6\"></Field>" +
+                        "<Field ID=\"32\" TYPE=\"6\"></Field>" +
+                        "<VARLIST>" +
+                            "<VARIABLE VARID=\"913\" FIELDTYPE=\"13\"></VARIABLE>" +
+                            "<VARIABLE VARID=\"217\" FIELDTYPE=\"11\"></VARIABLE>" +
+                        "</VARLIST>" +
+                    "</Layout>" +
+                    "<Layout>" +
+                        "<Field ID=\"33\" TYPE=\"6\"></Field>" +
+                        "<Field ID=\"34\" TYPE=\"6\"></Field>" +
+                        "<VARLIST>" +
+                            "<VARIABLE VARID=\"913\" FIELDTYPE=\"13\"></VARIABLE>" +
+                            "<VARIABLE VARID=\"217\" FIELDTYPE=\"11\"></VARIABLE>" +
+                        "</VARLIST>" +
+                    "</Layout>" +
+                "</Root>")
+        val inputStream4: InputStream =
+            ByteArrayInputStream(xml4.toByteArray(StandardCharsets.UTF_8))
+        val xmlFactory4 = XmlFactory.builder()
+            .xmlInputFactory(WstxInputFactory())
+            .xmlOutputFactory(WstxOutputFactory())
+            .build()
+        val xmlMapper4:XmlMapper = XmlMapper.builder(xmlFactory4).build()
+        var actualXmlObj4 = xmlMapper4.readTree(inputStream4)
+        xmlBranchPath = "/Layout/Field"
+        var subNode4 = actualXmlObj4.at(xmlBranchPath)
+        println("complex xml path1::subNode4.nodeType:${subNode4.nodeType}")
+        xmlBranchPath = "/Layout"
+        subNode4 = actualXmlObj4.at(xmlBranchPath)
+        when(subNode4.nodeType){
+            JsonNodeType.OBJECT -> println("complex xml path2::${subNode4.toPrettyString()}")
+            JsonNodeType.ARRAY -> subNode4.forEach{
+                println("complex xml path3::${it.toPrettyString()}")
+                xmlBranchPath = "/Field"
+                var level2Node = it.at(xmlBranchPath)
+                when(level2Node.nodeType){
+                    JsonNodeType.OBJECT -> println("complex xml path4::${subNode4.toPrettyString()}")
+                    JsonNodeType.ARRAY -> level2Node.forEach{
+                        println("complex xml path4::${it.toPrettyString()}")
+                    }
+                    else -> {
+                        println("it.nodeType:${it.nodeType}")
+                    }
+                }
+            }
+            else -> {
+                println("subNode4.nodeType:${subNode4.nodeType}")
+            }
+        }
+        //inner class to analyse nested path
+        var listStr = listOf<String>("/Layout","/Field")
+        Unpack.unpackNested(listStr,actualXmlObj4,0)
+        println("xml4 end.")
 
         /*
          * Dom parser

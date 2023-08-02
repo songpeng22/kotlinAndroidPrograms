ee21d67ae9cfaad267d6e03c4fb8a491d6a9a7e9
peng <songpeng24@msn.com>
2023-08-02 16:52:01 +0800

    add readme to 010_FragmentViewBinding

--- 010_FragmentViewBinding/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.kt
+++ 010_FragmentViewBinding/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.kt
@@ -3,6 +3,14 @@ package com.hfad.bitsandpizzas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * use View Binding to make a guessing game
+ * logic:
+ *
+ * new technics:
+ *
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

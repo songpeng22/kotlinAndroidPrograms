1f0ebd8c5c4a417785d1fe4a6f1a327c2fa6fdcb
peng <songpeng24@msn.com>
2023-08-02 13:30:06 +0800

    add readme to 005_scretMessage

--- 005_scretMessage/app/src/main/java/com/hfad/secretmessage/MainActivity.kt
+++ 005_scretMessage/app/src/main/java/com/hfad/secretmessage/MainActivity.kt
@@ -3,6 +3,20 @@ package com.hfad.secretmessage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * use navigation switching from fragment to fragment
+ * logic:
+ * setContentView(R.layout.activity_main) -> FragmentContainerView->app:navGraph -> res/navigation/nav_graph.xml
+ * default fragment container -> android:name="androidx.navigation.fragment.NavHostFragment"
+ * navigation graph -> app:navGraph="@navigation/nav_graph"
+ * new technics:
+ * Navigation
+ * new technics points:
+ * implementation "androidx.navigation:navigation-fragment-ktx:$nav_version"
+ * Navigation -> connect fragment using action
+ * Navigation -> findNavController
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

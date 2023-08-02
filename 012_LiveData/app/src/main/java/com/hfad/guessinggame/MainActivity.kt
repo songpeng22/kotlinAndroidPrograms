d5bea7ac6b8d9c3a837eb027cb55ed7d1f45bb56
peng <songpeng24@msn.com>
2023-08-02 17:03:07 +0800

    add readme to 012_LiveData

--- 012_LiveData/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
+++ 012_LiveData/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
@@ -3,6 +3,16 @@ package com.hfad.guessinggame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * use View Model and live data in Guessing game
+ * logic:
+ * activity_main -> nav_graph && NavHostFragment -> app:startDestination="@id/gameFragment"
+ * new technics:
+ * live data -> list of String like data which is life cycle controllable by owner
+ * new technic points:
+ * implementation "androidx.navigation:navigation-fragment-ktx:$nav_version"
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

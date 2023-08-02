5af36f05bab88925b8f9cd01f9327947f41b0c9d
peng <songpeng24@msn.com>
2023-08-02 16:55:30 +0800

    add readme to 011_GuessingGame

--- 011_GuessingGame/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
+++ 011_GuessingGame/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
@@ -3,6 +3,17 @@ package com.hfad.guessinggame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * use View Model redo guessing game
+ * logic:
+ *
+ * new technics:
+ *
+ * new technic points:
+ * ext.nav_version = "2.3.5"
+ * implementation "androidx.navigation:navigation-fragment-ktx:$nav_version"
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

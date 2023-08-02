b9414e2e74fa6af5634a4997fe4790e622d4a8b9
peng <songpeng24@msn.com>
2023-08-02 13:05:56 +0800

    add readme to 003_ContraintLayout_Flow

--- 003_ContraintLayout_Flow/app/src/main/java/com/hfad/myconstraintlayout/MainActivity.kt
+++ 003_ContraintLayout_Flow/app/src/main/java/com/hfad/myconstraintlayout/MainActivity.kt
@@ -3,6 +3,14 @@ package com.hfad.myconstraintlayout
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * layout -> ConstraintLayout
+ * logic:
+ *
+ * new technics:
+ * constraintlayout flow
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

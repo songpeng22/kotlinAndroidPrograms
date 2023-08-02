045d272edd39df795785df34e6d4abf2e1b2cbb4
peng <songpeng24@msn.com>
2023-08-02 17:05:05 +0800

    add readme to 013_DataBinding

--- 013_DataBinding/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
+++ 013_DataBinding/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
@@ -3,6 +3,16 @@ package com.hfad.guessinggame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * use Data Binding in Guessing game
+ * logic:
+ *
+ * new technics:
+ * Data Binding
+ * new technic points:
+ * buildFeatures.dataBinding = true
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

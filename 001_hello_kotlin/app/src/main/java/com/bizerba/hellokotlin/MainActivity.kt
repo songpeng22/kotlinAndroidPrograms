dae61857833c93392094c0d856f1fba760240c11
peng <songpeng24@msn.com>
2023-08-02 11:12:11 +0800

    add readme to 001_hello_kotlin

--- 001_hello_kotlin/app/src/main/java/com/bizerba/hellokotlin/MainActivity.kt
+++ 001_hello_kotlin/app/src/main/java/com/bizerba/hellokotlin/MainActivity.kt
@@ -5,6 +5,10 @@ import android.os.Bundle
 
 import android.util.Log
 
+/*
+ * what this project do:
+ * build a hello world project with 'kotlin android' insead of 'java android'
+ * */
 class MainActivity : AppCompatActivity() {
     private var TAG : String = "Hello_Kotlin"
     override fun onCreate(savedInstanceState: Bundle?) {

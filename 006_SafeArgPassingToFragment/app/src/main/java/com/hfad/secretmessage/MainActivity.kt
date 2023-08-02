7dcde8a1e9c9c92de16f96c70afa2aec9159cb15
peng <songpeng24@msn.com>
2023-08-02 15:14:04 +0800

    add readme to 006_SafeArgPassingToFragment

--- 006_SafeArgPassingToFragment/app/src/main/java/com/hfad/secretmessage/MainActivity.kt
+++ 006_SafeArgPassingToFragment/app/src/main/java/com/hfad/secretmessage/MainActivity.kt
@@ -3,6 +3,14 @@ package com.hfad.secretmessage
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * transfer message from fragment to fragment
+ * logic:
+ * input message -> click button -> transfer
+ * new technics:
+ * argument -> EncryptFragmentArgs
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

1578a09a552bc1065dc76d3426fb63296b6e2684
peng <songpeng24@msn.com>
2023-08-02 17:07:09 +0800

    add readme to 014_database

--- 014_database/app/src/main/java/com/hfad/tasks/MainActivity.kt
+++ 014_database/app/src/main/java/com/hfad/tasks/MainActivity.kt
@@ -3,6 +3,15 @@ package com.hfad.tasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * database -> Room
+ * logic:
+ *
+ * new technics:
+ *
+ * new technic points:
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

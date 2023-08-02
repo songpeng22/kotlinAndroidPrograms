123d6cb87b4147c2ed3fc44864cac627fe42d526
peng <songpeng24@msn.com>
2023-08-02 16:33:29 +0800

    add readme to 008_BitsandPizzas

--- 008_BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.kt
+++ 008_BitsandPizzas/app/src/main/java/com/hfad/bitsandpizzas/MainActivity.kt
@@ -3,6 +3,16 @@ package com.hfad.bitsandpizzas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * Material themes to show some components usage
+ * logic:
+ * setContentView(R.layout.activity_main) -> CoordinatorLayout -> OrderFragment
+ * new technics:
+ * tool bar, scroll collapse
+ * checkbox/radio button/chips
+ * floating action button
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

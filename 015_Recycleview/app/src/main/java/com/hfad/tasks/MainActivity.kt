8c6cfe453d6161ba6855b3784939f7dd446b0b9e
peng <songpeng24@msn.com>
2023-08-03 09:01:29 +0800

    add readme to add 015_Recycleview

--- 015_Recycleview/app/src/main/java/com/hfad/tasks/MainActivity.kt
+++ 015_Recycleview/app/src/main/java/com/hfad/tasks/MainActivity.kt
@@ -3,6 +3,17 @@ package com.hfad.tasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * read database and show data in RecyclerView
+ * logic:
+ * activity_main.xml -> FragmentContainerView -> TasksFragment -> RecyclerView
+ * new technics:
+ * Recycle View
+ * new technic points:
+ * implementation "androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version"
+ * implementation "androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version"
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

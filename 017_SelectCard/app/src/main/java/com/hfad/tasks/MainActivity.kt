2536b518d21b6bfdf780721ae2a5396a410fe11d
peng <songpeng24@msn.com>
2023-08-03 09:16:21 +0800

    add readme to 017_SelectCard

--- 017_SelectCard/app/src/main/java/com/hfad/tasks/MainActivity.kt
+++ 017_SelectCard/app/src/main/java/com/hfad/tasks/MainActivity.kt
@@ -3,6 +3,16 @@ package com.hfad.tasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * clicking on Recycle View, it will navigate to edit fragment
+ * logic:
+ *
+ * new technics:
+ *
+ * new technic points:
+ * viewModel.navigateToTask.observe
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

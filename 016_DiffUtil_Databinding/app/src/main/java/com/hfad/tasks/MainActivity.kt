a97c4982ae375c31b8beb084c8525bfeb8e4a837
peng <songpeng24@msn.com>
2023-08-03 09:05:16 +0800

    add readme to 016_DiffUtil_Databinding

--- 016_DiffUtil_Databinding/app/src/main/java/com/hfad/tasks/MainActivity.kt
+++ 016_DiffUtil_Databinding/app/src/main/java/com/hfad/tasks/MainActivity.kt
@@ -3,6 +3,16 @@ package com.hfad.tasks
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * like a virtual list, DiffUtil tells Recycle View where the difference of two list, make it more efficient
+ * logic:
+ *
+ * new technics:
+ * DiffUtil
+ * new technic points:
+ *
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

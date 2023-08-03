a4a649f5bbd3657df1cd9a85c6d15fd19906eaa0
peng <songpeng24@msn.com>
2023-08-03 09:50:39 +0800

    add readme to 019_AndroidProgramComposable

--- 019_AndroidProgramComposable/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
+++ 019_AndroidProgramComposable/app/src/main/java/com/hfad/guessinggame/MainActivity.kt
@@ -3,6 +3,16 @@ package com.hfad.guessinggame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/*
+ * what this project do:
+ * interact compose with view
+ * logic:
+ * activity_main.xml -> navigation/nav_graph.xml -> app:startDestination="@id/gameFragment" -> GameFragment
+ * new technics:
+ * put viewModel into Compose View
+ * new technic points:
+ * GameFragmentContent(viewModel)
+ * */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
